570e6bf5c7e98ad24ecf53a2ddb7aaaf8d95ce90
Henriquelay <henriquelayber@gmail.com>
2021-02-26 17:02:21 -0300

    Usando função dada

--- simpleSort/selection_sort.c
+++ simpleSort/selection_sort.c
@@ -3,9 +3,7 @@
 void sort(Item* a, int lo, int hi) {
     for (int i = lo; i < hi;i++) {      // For every element
         for (int j = i; j < hi; j++) {  // Search the ones later
-            if (a[j] < a[i]) {         // If it's smaller than the first
-                exch(a[j], a[i]);      // Swap them
-            }
+            compexch(a[i], a[j]);         // If it's smaller than the first, swap them
         }
     }
 }
