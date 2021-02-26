2a9f5dc679b3b89bbf6806c933007122b5198de1
Henriquelay <henriquelayber@gmail.com>
2021-02-26 17:09:18 -0300

    Fixed for compexch that makes sense

--- simpleSort/item.h
+++ simpleSort/item.h
@@ -5,6 +5,6 @@ typedef int Item;
 #define key(A)          (A)
 #define less(A, B)      (key(A) < key(B))
 #define exch(A, B)      { Item t = A; A = B; B = t; }
-#define compexch(A, B)  if (less(B, A)) exch(A, B)
+#define compexch(A, B)  if (less(A, B)) exch(A, B)
 
 #endif
