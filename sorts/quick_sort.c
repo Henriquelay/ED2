4e6bbd7859a413e62ee21f060d1e8d912f1d6ced
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-03-25 04:56:14 -0300

    V3

--- sorts/quick_sort.c
+++ sorts/quick_sort.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <sys/time.h>
+
 #include "item.h"
 
 #define CUTOFF (15)
@@ -13,11 +16,33 @@
 //     }
 // }
 
+int randomBetween(int lo, int hi) {
+    return lo + (unsigned int) drand48() % hi;
+}
+
 void insertionSort(Item* a, int lo, int hi) {
     for (int i = lo; i < hi; i++) {      // Para todos os elementos
         for (int j = i; j > lo; j--) {   // Para os ordenados
             compexch(a[j], a[j - 1]);
         }
+    }
+}
+
+
+int median_of_3(Item* a, int lo, int hi) {
+    // TODO srand only once
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    srand48(tv.tv_usec);
+
+    int medianOf[3] = { randomBetween(lo, hi), randomBetween(lo, hi), randomBetween(lo, hi) };
+
+    if (medianOf[0] <= medianOf[1]) {
+        if(medianOf[1] <= medianOf[2]) return medianOf[1];
+        return medianOf[2];
+    }  else {
+        if(medianOf[1] <= medianOf[2]) return medianOf[0];
+        return medianOf[1];
     }
 }
 
@@ -37,8 +62,8 @@ int partition(Item* a, int lo, int hi) {
 }
 
 void sort(Item* a, int lo, int hi) {
-    if (hi <= lo + CUTOFF - 1) {
-        insertionSort(a, lo, hi);
+    if (hi <= lo) {
+        // insertionSort(a, lo, hi);
         return;
     }
     int j = partition(a, lo, hi);
