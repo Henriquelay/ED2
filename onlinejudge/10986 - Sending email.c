f42c8f0eaf67a12acdd3797c6103ab9c68f668f4
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-04-13 17:52:35 -0300

    ANSIzado 1948 zebong edition vamo q vamo

--- onlinejudge/10986 - Sending email.c
+++ onlinejudge/10986 - Sending email.c
@@ -2,73 +2,73 @@
 #include <stdlib.h>
 #include <limits.h>
 
-typedef struct Edge {
+typedef struct Item_Edge {
     unsigned int id;          /* identificador do nó */
     unsigned int weight;     /* valor associado ao nó */
-    struct Edge* to;     /* valor associado ao nó destino */
-} Edge;
+    unsigned int to;     /* valor associado ao nó destino */
+} Item_Edge;
 
-typedef struct PQ {
-    Edge** pq;
-    int* map;
+typedef struct PQ_Vertex {
+    Item_Edge** edges;
+    unsigned int* map;
     unsigned int size;
-} PQ;
+} PQ_Vertex;
 
 #define id(A)           (A->id)                           /* retorna identificador do nó */
 #define value(A)        (A->weight)                       /* retorna valor do nó */
-#define to(A)           (id(A->to))                       /* retorna id do nó destino */
+#define to(A)           (A->to)                       /* retorna id do nó destino */
 #define more(A, B)      (value(A) > value(B))          /* compara nós, por valor */
 /* #define exch(A, B)      { Item t = A; A = B; B = t; }  // troca dois nós */
-void exch(Edge** a, Edge** b) {
-    Edge* T = *a;
+void exch(Item_Edge** a, Item_Edge** b) {
+    Item_Edge* T = *a;
     *a = *b;
     *b = T;
 }
 
-Edge make_item(unsigned int id, unsigned int value, Edge* to) {
-    Edge t;
+Item_Edge make_item(unsigned int id, unsigned int value, unsigned int to) {
+    Item_Edge t;
     t.id = id;
     t.weight = value;
     t.to = to;
     return t;
 }
 
-void print_item(Edge* item) {
+void print_item(Item_Edge* item) {
     if (item == NULL) { printf("NULL"); return; }
     printf("%u-", id(item));
     printf("%u", value(item));
     printf("->%u", to(item));
 }
 
-Edge* init_item(unsigned int id, unsigned int value, Edge* to) {
-    Edge* newItem = (Edge*)malloc(sizeof(Edge));
+Item_Edge* init_item(unsigned int id, unsigned int value, unsigned int to) {
+    Item_Edge* newItem = malloc(sizeof * newItem);
     *newItem = make_item(id, value, to);
     return newItem;
 }
 
-void swap(PQ* pq, unsigned int i, unsigned int j) {
-    exch(&pq->pq[i], &pq->pq[j]);
-    /* pq->map[id(pq->pq[i])] = i; */
-    /* pq->map[id(pq->pq[j])] = j; */
+void swap(PQ_Vertex* pq, unsigned int i, unsigned int j) {
+    exch(&pq->edges[i], &pq->edges[j]);
+    pq->map[id(pq->edges[i])] = i;
+    pq->map[id(pq->edges[j])] = j;
 }
 
-void fix_up(PQ* pq, unsigned int k) {
-    while (k > 1 && more(pq->pq[k / 2], pq->pq[k])) {
-        if (pq && pq->pq) {
+void fix_up(PQ_Vertex* pq, unsigned int k) {
+    while (k > 1 && more(pq->edges[k / 2], pq->edges[k])) {
+        if (pq && pq->edges) {
         }
         swap(pq, k, k / 2);
         k = k / 2;
     }
 }
 
-void fix_down(PQ* pq, unsigned int parent) {
+void fix_down(PQ_Vertex* pq, unsigned int parent) {
     while (2 * parent <= pq->size) {
         unsigned int child = 2 * parent;
         /* child < pq->size checks if there's a position after child, since PQ starts at 1; */
-        if (child < pq->size && more(pq->pq[child], pq->pq[child + 1])) {
+        if (child < pq->size && more(pq->edges[child], pq->edges[child + 1])) {
             child++;
         }
-        if (!more(pq->pq[parent], pq->pq[child])) {
+        if (!more(pq->edges[parent], pq->edges[child])) {
             break;
         }
         swap(pq, parent, child);
@@ -76,87 +76,86 @@ void fix_down(PQ* pq, unsigned int parent) {
     }
 }
 
-PQ* PQ_init(unsigned int maxSize) {
-    PQ* pq = (PQ*)malloc(sizeof(PQ));
-    pq->pq = (Edge**)calloc(maxSize + 1, sizeof(Edge*));
-    pq->map = (unsigned int*)malloc((maxSize + 1) * sizeof(unsigned int));
+PQ_Vertex* PQ_init(unsigned int maxSize) {
+    PQ_Vertex* pq = malloc(sizeof * pq);
+    pq->edges = malloc(sizeof * pq->edges * (maxSize + 1));
+    pq->map = malloc(sizeof * pq->map * (maxSize + 1));
     pq->size = 0;
     return pq;
 }
 
-void PQ_print(PQ* pq) {
+void PQ_print(PQ_Vertex* pq) {
     if (pq == NULL) {
         puts("It's NULL");
         return;
     }
-    printf("PQ=%p map=%p size=%u\n", (void*)pq->pq, (void*)pq->map, pq->size);
+    printf("PQ=%p map=%p size=%u\n", (void*)pq->edges, (void*)pq->map, pq->size);
     /* This implementation skips index 0 */
     unsigned int i;
     for (i = 0; i <= pq->size; i++) {
         printf("[%u] ", i);
-        print_item(pq->pq[i]);
+        print_item(pq->edges[i]);
         printf("\n");
     }
 }
 
-/* FIXME */
-void PQ_insert(PQ* pq, Edge* v) {
+void PQ_insert(PQ_Vertex* pq, Item_Edge* v) {
     pq->size++;
-    pq->pq[pq->size] = v;
+    pq->edges[pq->size] = v;
     pq->map[id(v)] = pq->size;
     fix_up(pq, pq->size);
 }
 
-Edge* PQ_min(PQ* pq) {
-    return pq->pq[1];
+Item_Edge* PQ_min(PQ_Vertex* pq) {
+    return pq->edges[1];
 }
 
-Edge* PQ_delmin(PQ* pq) {
-    Edge* min = PQ_min(pq);
+Item_Edge* PQ_delmin(PQ_Vertex* pq) {
+    Item_Edge* min = PQ_min(pq);
     swap(pq, 1, pq->size);
     pq->size--;
     fix_down(pq, 1);
     return min;
 }
 
-void PQ_decrease_key(PQ* pq, unsigned int id, unsigned int value) {
+void PQ_decrease_key(PQ_Vertex* pq, unsigned int id, unsigned int value) {
     unsigned int i = pq->map[id];
-    value(pq->pq[i]) = value;
+    value(pq->edges[i]) = value;
     fix_up(pq, i);
 }
 
-char PQ_empty(PQ* pq) {
+char PQ_empty(PQ_Vertex* pq) {
     return pq->size == 0;
 }
 
-char PQ_exists(PQ* pq, unsigned int key) {
+char PQ_exists(PQ_Vertex* pq, unsigned int key) {
     unsigned int i;
     for (i = 1; i <= pq->size; i++) {
-        if (id(pq->pq[i]) == key) return 1;
+        if (id(pq->edges[i]) == key) return 1;
     }
     return 0;
 }
 
-char PQ_existsAddr(PQ* pq, void* keyAddr) {
+char PQ_existsAddr(PQ_Vertex* pq, void* keyAddr) {
     unsigned int i;
     for (i = 0; i < pq->size; i++) {
-        if (&(pq->pq[i]) == keyAddr) return 1;
+        if (&(pq->edges[i]) == keyAddr) return 1;
     }
     return 0;
 }
 
-unsigned int PQ_size(PQ* pq) {
+unsigned int PQ_size(PQ_Vertex* pq) {
     return pq->size;
 }
 
 /* Frees the Items inside too */
-void PQ_finish(PQ* pq) {
+void PQ_finish(PQ_Vertex* pq) {
     if (pq == NULL) return;
     unsigned int i;
-    for (i = 0; i < pq->size;) {
-        free(pq->pq[++i]);
+    for (i = 1; i <= pq->size; i++) {
+        free(pq->edges[i]);
     }
-    free(pq->pq);
+    free(pq->edges);
     free(pq->map);
     free(pq);
     pq = NULL;
@@ -164,7 +163,7 @@ void PQ_finish(PQ* pq) {
 
 /*/////////////////////////////////// */
 
-unsigned int* dijkstra(PQ** vertex, unsigned int nVert, int starting) {
+unsigned int* dijkstra(PQ_Vertex** vertex, unsigned int nVert, int starting) {
     /* If starting node has no edges */
     /* Will be useful quitting early to not allocate anything */
     if (vertex[starting] == NULL) {
@@ -178,53 +177,55 @@ unsigned int* dijkstra(PQ** vertex, unsigned int nVert, int starting) {
     /* To mark visited nodes */
     char visited[nVert];
     /* Internal Dijkstra's control */
-    PQ* dijEdges = PQ_init(3000);
+    PQ_Vertex* dijEdges = PQ_init(3000);
 
-    /* Initializing arrays */
     unsigned int i;
     for (i = 1; i <= vertex[starting]->size; i++) {
-        PQ_insert(dijEdges, vertex[starting]->pq[i]);
+        PQ_insert(dijEdges, vertex[starting]->edges[i]);
     }
+    /* Inserting only starting node */
     for (i = 0; i < nVert; i++) {
         /* I    nitializes with maximum distance, gets replaced on first iteration */
         distances[i] = UINT_MAX;
         visited[i] = 0;
 
-        /* Inserting only starting node */
         /* Inserting all edges from every vertice */
-        /* unsigned int j; for (j = 1; j <= vertex[i]->size; j++) { */
-        /*     PQ_insert(dijVertex[i], vertex[i]->pq[j]); */
-        /* } */
+        /* unsigned int j; for (j = 1; j <= vertex[i]->size; j++) {
+            PQ_insert(dijEdges, vertex[i]->pq[j]);
+        } */
     }
     /* A vertice's distance to itself is 0. */
     distances[starting] = 0;
 
 
-    /* puts("After starting everything:"); */
-    /* PQ_print(dijEdges); */
+    /* puts("\nAfter starting everything, before dijkstra:");
+    PQ_print(dijEdges); */
 
-    /* TODO */
     while (!PQ_empty(dijEdges)) {
-        /* puts("Current state:"); */
-        /* PQ_print(dijEdges); */
-        Edge* edge = PQ_delmin(dijEdges);
-        /* printf("Current edge:"); */
-        /* print_item(edge); */
-        /* printf("\nDistances:["); */
-        /* unsigned int k; for (k = 0; k < nVert; k++) { */
-        /*     printf("%u ", distances[k]); */
-        /* } */
-        /* puts("]"); */
+        /* puts("Current state:");
+        PQ_print(dijEdges); */
+        Item_Edge* edge = PQ_delmin(dijEdges);
+        /* puts("After delmin:");
+        PQ_print(dijEdges);
+        printf("Current edge:");
+        print_item(edge);
+        printf("\nDistances:[");
+        unsigned int k; for (k = 0; k < nVert; k++) {
+            printf("%u ", distances[k]);
+        } 
+        puts("]"); */
         unsigned int newDistance = distances[id(edge)] == UINT_MAX ? value(edge) : distances[id(edge)] + value(edge);
 
-        /* printf("NewDistance from %u to %u: %u\n", id(edge), to(edge), newDistance); */
+        /* printf("NewDistance from %u to %u: %u\n", id(edge), to(edge),  newDistance); */
         if (newDistance < distances[to(edge)]) {
             /* puts("Updating distance"); */
             distances[to(edge)] = newDistance;
             if (!visited[to(edge)]) {
+                /* puts("Inserting node:"); */
+                /* PQ_print(vertex[to(edge)]); */
                 unsigned int j;
-                for (j = 1; j <= vertex[starting]->size; j++) {
-                    PQ_insert(dijEdges, vertex[to(edge)]->pq[j]);
+                for (j = 1; j <= vertex[to(edge)]->size; j++) {
+                    PQ_insert(dijEdges, vertex[to(edge)]->edges[j]);
                 }
             }
         }
@@ -234,7 +235,7 @@ unsigned int* dijkstra(PQ** vertex, unsigned int nVert, int starting) {
     }
 
 
-    free(dijEdges->pq);
+    free(dijEdges->edges);
     free(dijEdges->map);
     free(dijEdges);
     return distances;
@@ -255,7 +256,7 @@ int main(int argc, char* argv[]) {
 
         /* printf("n=%u m=%u S=%u T=%u\n", n, m, S, T); */
 
-        PQ* pqMap[n];
+        PQ_Vertex* pqMap[n];
         unsigned int j;
         for (j = 0; j < n; j++) {
             pqMap[j] = NULL;
@@ -270,9 +271,8 @@ int main(int argc, char* argv[]) {
             if (pqMap[Scur] == NULL) pqMap[Scur] = PQ_init(1000);
             if (pqMap[Tcur] == NULL) pqMap[Tcur] = PQ_init(1000);
 
-            Edge* SItem = init_item(Scur, w, NULL);
-            Edge* TItem = init_item(Tcur, w, SItem);
-            SItem->to = TItem;
+            Item_Edge* SItem = init_item(Scur, w, Tcur);
+            Item_Edge* TItem = init_item(Tcur, w, Scur);
 
             PQ_insert(pqMap[Scur], SItem);
             PQ_insert(pqMap[Tcur], TItem);
@@ -299,10 +299,10 @@ int main(int argc, char* argv[]) {
         /* } */
         /* printf("]\n"); */
 
-        if (distances != NULL) {
-            printf("%u", distances[T]);
-        } else {
+        if (distances == NULL || distances[T] == UINT_MAX) {
             printf("unreachable");
+        } else {
+            printf("%u", distances[T]);
         }
         puts("");
 
